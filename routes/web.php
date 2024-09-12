5c4d055b32f64a858a90fd3e5f7d0bc7b2798888
Tomie Sayaka <tomie_sayaka@giginc.co.jp>
2024-09-12 15:20:03 +0900

    リダイレクト

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 })->middleware(RedirectIfAuthenticated::class); 
 */
 
+Route::redirect('/', '/home');
 Route::get('/home', [TweetController::class, 'index'])->name('tweets.index');
 
 Route::middleware('auth')->group(function () {
